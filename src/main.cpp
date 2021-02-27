75fe13cf6cb575b5c55d1356fe85db9988b8e5e7
leo <leo.buchman@lenel.com>
2021-02-27 04:34:35 -0500

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
+#include <string.h>
+#include <iostream>
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
 
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char **argv) {
+    
+  int baudrate = 115200;
+  char deviceFile[32] = "/dev/ttyOSB0";
+
+  int c;
+
+  opterr = 0;
+
+
+  while ((c = getopt (argc, argv, "pb:h")) != -1)
+    switch (c)
+      {
+      case 'b':
+        baudrate = std::stoi(optarg);
+        break;
+      case 'p':
+        strcpy(deviceFile, optarg);
+        break;
+      case '?':
+        return 1;
+      default:
+        abort ();
+      }
+
+
+return 0;   
     
 }
