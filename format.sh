da6056ceb938152df991f4504cfabd076ff034c9
Leo Buchman <leo.buchman@lenel.com>
2021-03-10 19:11:30 -0500

    work

--- format.sh
+++ format.sh
@@ -2,8 +2,8 @@
 
 STYLE=java
 OPTIONS_STYLE="--indent-preprocessor --verbose --indent-cases --indent-switches --indent-classes --break-elseifs --add-brackets --unpad-paren --convert-tabs --break-closing-brackets --break-blocks --indent-col1-comments --indent-switches --indent-classes --pad-oper"
-astyle -n --recursive --style=${STYLE} ${OPTIONS_STYLE} "src/*.cpp"
-astyle -n --recursive --style=${STYLE} ${OPTIONS_STYLE} "inc/*.h"
-astyle -n --recursive --style=${STYLE} ${OPTIONS_STYLE} "extLib/*.cpp"
-astyle -n --recursive --style=${STYLE} ${OPTIONS_STYLE} "extLib/*.h"
+astyle -n --recursive --style=${STYLE} ${OPTIONS_STYLE} "../src/*.cpp"
+astyle -n --recursive --style=${STYLE} ${OPTIONS_STYLE} "../inc/*.h"
+astyle -n --recursive --style=${STYLE} ${OPTIONS_STYLE} "../extLib/*.cpp"
+astyle -n --recursive --style=${STYLE} ${OPTIONS_STYLE} "../*.h"
 
